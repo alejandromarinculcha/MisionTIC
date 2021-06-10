490675da0bbd521eefd07acb878b79d3f2e0af35
Alejandro Marin Culcha <alejandro.marin_cul@uao.edu.co>
2021-06-10 09:31:25 -0500

    Add files via upload

---
+++ Caracteres.py
@@ -0,0 +1,44 @@
+# EJERCICIO 1
+def conversion1(x):
+
+    y = chr(x)
+
+    if (y == 'a' or y == 'e' or y == 'i' or y == 'o' or y == 'u'):
+        return("El número " + str(x) + " si corresponde al ascii de una vocal mínuscula que es " + y)
+    else:
+        return("El número " + str(x) + " No corresponde al ascii de una vocal mínuscula " + "corresponde a " + y)
+
+
+x = int(input("Ingrese un número: "))
+
+print(conversion1(x))
+
+
+# EJERCICIO 2
+def converison2():
+
+    x = (input("Ingrese un carácter de longitud 1: "))
+
+    y = ord(x)
+
+    if (y % 2 == 0):
+        print("El número:", y, "ES PAR")
+    else:
+        print("El número:", y, "ES IMPAR")
+
+
+converison2()
+
+
+# EJERCICIO 3
+def conversion3(x):
+
+    if (str.isdigit(x)):
+        print("La cadena: " + x + " es un digito.")
+    else:
+        print("La cadena: " + x + " no es un digito.")
+
+
+x = (input("Ingrese una cadena: "))
+
+conversion3(x)
